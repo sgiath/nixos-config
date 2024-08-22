f754ff2a941c3a6ca4bdd421ad84e50440bfcea0
sgiath <sgiath@sgiath.dev>
2024-08-22 07:44:29 +0000

    flake update
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- nixos/wayland.nix
+++ nixos/wayland.nix
@@ -12,11 +12,11 @@
   };
 
   config = lib.mkIf config.sgiath.wayland.enable {
-    programs.hyprland = {
-      enable = true;
-      xwayland.enable = true;
-      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-    };
+    # programs.hyprland = {
+    #   enable = true;
+    #   xwayland.enable = true;
+    #   package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    # };
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
     services.greetd = {
