d2956fe019477a4c713ba442e4ce79d3da0c6228
sgiath <sgiath@sgiath.dev>
2024-08-24 19:03:16 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- work/default.nix
+++ work/default.nix
@@ -2,9 +2,15 @@
 
 {
   imports = [ ./aws.nix ];
+
   home.packages = with pkgs; [
     slack
     google-chrome
     insomnia
   ];
+
+  # for wayland screensharing
+  xdg.configFile."chrome-flags.conf".text = ''
+    --ozone-platform-hint=auto
+  '';
 }
