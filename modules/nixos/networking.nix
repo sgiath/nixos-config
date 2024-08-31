fe44da2445c4bc29b5b23e76800d03f823e1197e
sgiath <sgiath@sgiath.dev>
2024-08-31 22:25:25 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -14,14 +14,21 @@
         "192.168.1.5" = [ "nas.sgiath" ];
         "192.168.1.150" = [ "mix.sgiath" ];
       };
-      nameservers =
-        if config.sgiath.networking.localDNS.enable then
-          [ "192.168.1.2" ]
-        else
-          [ "8.8.8.8" "8.8.4.4" ];
-      networkmanager.enable = true;
+
+      resolved.enable = false;
+      resolvconf.enable = lib.mkForce false;
+      dhcpcd.extraConfig = "nohook resolv.conf";
+      networkmanager = {
+        enable = true;
+        dns = "none";
+      };
       firewall.enable = false;
     };
+    environment.etc."resolv.conf".text =
+      if config.sgiath.networking.localDNS.enable then
+        "nameserver 192.168.1.2\n"
+      else
+        "nameserver 8.8.8.8\nnameserver 8.8.4.4\n";
 
     users.users.${userSettings.username}.extraGroups = [ "networkmanager" ];
   };
