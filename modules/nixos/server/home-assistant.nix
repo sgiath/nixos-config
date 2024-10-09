ac9849030f8d6c61cd2aedc9c2590b916df150e3
sgiath <sgiath@sgiath.dev>
2024-10-09 19:18:26 +0000

    server changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/server/home-assistant.nix
+++ modules/nixos/server/home-assistant.nix
@@ -15,24 +15,18 @@
         http3_hq = true;
         quic = true;
 
-        # static files
         locations."/" = {
           proxyWebsockets = true;
           proxyPass = "http://127.0.0.1:8123";
-          # does not work with X-Forwarded-For
-          # recommendedProxySettings = false;
-          # extraConfig = ''
-          #   proxy_set_header        Host $host;
-          #   proxy_set_header        X-Real-IP $remote_addr;
-          #   # proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
-          #   proxy_set_header        X-Forwarded-Proto $scheme;
-          #   proxy_set_header        X-Forwarded-Host $host;
-          #   proxy_set_header        X-Forwarded-Server $host;
-          # '';
         };
       };
 
       home-assistant = {
+        extraPackages =
+          python3Packages: with python3Packages; [
+            psycopg2
+            gtts
+          ];
         config = {
           http = {
             server_host = [ "127.0.0.1" ];
