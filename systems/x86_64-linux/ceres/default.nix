4ebb652b24a3cf120facbff619f6db00937731ae
sgiath <sgiath@sgiath.dev>
2024-10-11 09:38:16 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- systems/x86_64-linux/ceres/default.nix
+++ systems/x86_64-linux/ceres/default.nix
@@ -30,22 +30,22 @@
     };
   };
 
-  users.users.sgiath.extraGroups = [ "libvirtd" ];
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [
-          (pkgs.OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-          }).fd
-        ];
-      };
-    };
-  };
+  # users.users.sgiath.extraGroups = [ "libvirtd" ];
+  # virtualisation.libvirtd = {
+  #   enable = true;
+  #   qemu = {
+  #     package = pkgs.qemu_kvm;
+  #     runAsRoot = true;
+  #     swtpm.enable = true;
+  #     ovmf = {
+  #       enable = true;
+  #       packages = [
+  #         (pkgs.OVMF.override {
+  #           secureBoot = true;
+  #           tpmSupport = true;
+  #         }).fd
+  #       ];
+  #     };
+  #   };
+  # };
 }
