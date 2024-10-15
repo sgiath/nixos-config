f99dfc86c9c123d38ab0ca9e7f5b3303df3b0b98
sgiath <sgiath@sgiath.dev>
2024-10-15 20:56:52 +0000

    server changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/server/search.nix
+++ modules/nixos/server/search.nix
@@ -15,11 +15,20 @@ in
         general = {
           instance_name = "sgiath";
           contact_url = "mailto:search@sgiath.dev";
-          twitter_url = "https://x.com/SgiathDev";
+        };
+        search = {
+          autocomplete = "duckduckgo";
+          default_lang = "en";
         };
         server = {
+          bind_address = "127.0.0.1";
           port = 8080;
+          base_url = "https://search.sgiath.dev/";
           secret_key = secrets.searx_secret;
+          http_protocol_version = "1.1";
+        };
+        outgoing = {
+          request_timeout = 5.0;
         };
       };
     };
