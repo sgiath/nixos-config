afaad52f9c69fdbec2dfb6f1094ee7cda824d542
sgiath <sgiath@sgiath.dev>
2024-11-07 08:03:46 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- flake.nix
+++ flake.nix
@@ -52,8 +52,7 @@
     };
 
     stylix = {
-      url = "github:danth/stylix";
-      rev = "762c07ee10b381bc8e085be5b6c2ec43139f13b0";
+      url = "github:danth/stylix/762c07ee10b381bc8e085be5b6c2ec43139f13b0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
