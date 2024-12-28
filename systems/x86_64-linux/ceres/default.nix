ee36d4037e897b4e124c404c45fb989e69aa3824
sgiath <sgiath@sgiath.dev>
2024-12-28 20:45:00 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- systems/x86_64-linux/ceres/default.nix
+++ systems/x86_64-linux/ceres/default.nix
@@ -26,6 +26,8 @@ in
   services = {
     ollama = {
       enable = true;
+      acceleration = "rocm";
+      host = "0.0.0.0";
       environmentVariables = {
         OLLAMA_ORIGINS = "*";
       };
