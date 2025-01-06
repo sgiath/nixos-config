8f2e2f0f435dd9c1f31658a7d1bf13317bdbc1b8
sgiath <sgiath@sgiath.dev>
2025-01-06 13:52:54 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/sgiath/amd-gpu.nix
+++ modules/nixos/sgiath/amd-gpu.nix
@@ -1,27 +1,22 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 {
   config = lib.mkIf (config.sgiath.enable && (config.sgiath.gpu == "amd")) {
-    boot = {
-      initrd.kernelModules = [ "amdgpu" ];
-      kernelModules = [ "kvm-amd" ];
-    };
-
+    boot.kernelModules = [ "kvm-amd" ];
     services.xserver.videoDrivers = [ "amdgpu-pro" ];
 
-    systemd.tmpfiles.rules = [ "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}" ];
-
-    hardware.graphics = {
-      extraPackages = with pkgs; [
-        rocmPackages.clr
-        rocmPackages.clr.icd
-        amdvlk
-      ];
-      extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
+    hardware = {
+      amdgpu = {
+        initrd.enable = true;
+        opencl.enable = true;
+        amdvlk = {
+          enable = true;
+          support32Bit.enable = true;
+        };
+      };
     };
   };
 }
