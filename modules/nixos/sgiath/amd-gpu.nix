58cc20a4e74dac6fdcc37e2f052054231ba5d863
sgiath <sgiath@sgiath.dev>
2025-01-26 10:07:36 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/sgiath/amd-gpu.nix
+++ modules/nixos/sgiath/amd-gpu.nix
@@ -1,12 +1,15 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 {
   config = lib.mkIf (config.sgiath.enable && (config.sgiath.gpu == "amd")) {
     boot.kernelModules = [ "kvm-amd" ];
     services.xserver.videoDrivers = [ "amdgpu" ];
+
+    environment.systemPackages = with pkgs; [ rocmPackages.rocm-smi ];
 
     hardware = {
       amdgpu = {
