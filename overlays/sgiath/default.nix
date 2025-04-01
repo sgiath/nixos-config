f00075b936ec50f6dbc6cbdaf09fefe318e2a525
sgiath <sgiath@sgiath.dev>
2025-04-01 07:45:57 +0000

    flake update
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- overlays/sgiath/default.nix
+++ overlays/sgiath/default.nix
@@ -34,6 +34,7 @@ in
   factorio = pkgs-master.factorio-space-age-experimental;
 
   # get open-webui updates sooner
+  # open-webui = pkgs-master.open-webui;
   open-webui = pkgs-drupol.open-webui;
 
   # broken on unstable for me
