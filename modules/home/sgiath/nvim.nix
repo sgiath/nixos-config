48a1f1cef293fffed9fa93613a6333b3e3f614ab
sgiath <sgiath@sgiath.dev>
2025-05-20 13:33:08 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/nvim.nix
+++ modules/home/sgiath/nvim.nix
@@ -1,6 +1,62 @@
-{config, lib, pkgs, ...}:
 {
-    programs.nixvim = {
-      enable = true;
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  options.programs.nvim = {
+    enable = lib.mkEnableOption "Neovim";
+  };
+
+  config = lib.mkIf config.programs.nvim.enable {
+    home.sessionVariables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
     };
+
+    # nixd LSP
+    home.packages = with pkgs; [
+      # base deps
+      neovim
+
+      # Zig
+      zig
+      zls
+
+      # Nix
+      nixd
+      nixfmt-rfc-style
+
+      # Lua
+      lua-language-server
+      stylua
+
+      # Markdown formatter
+      # https://github.com/executablebooks/mdformat
+      python312Packages.mdformat
+      python312Packages.mdformat-gfm
+      python312Packages.mdformat-frontmatter
+      python312Packages.mdformat-footnote
+
+      # shell
+      shfmt
+
+      # general formatter
+      codespell
+    ];
+
+    # ripgrep
+    programs.ripgrep.enable = true;
+
+    # config files
+    xdg = {
+      enable = true;
+      configFile.nvim = {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/modules/home/sgiath/nvim";
+        recursive = true;
+      };
+    };
+  };
 }
