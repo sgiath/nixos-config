e0d064ae63ce093340c14fafd8cad6e63acdf2dd
sgiath <sgiath@sgiath.dev>
2025-06-24 08:42:36 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/hyprland.nix
+++ modules/home/sgiath/hyprland.nix
@@ -34,9 +34,9 @@
 
         monitor = [
           # Desktop
-          "DP-1,highres,0x2560,1"
-          "DP-3,highres,0x1120,1"
-          "DP-2,highres,3440x0,1,transform,1"
+          "DP-1,5120x1440@240,0x2560,1"
+          "DP-3,3440x1440@165,0x1120,1"
+          "DP-2,2560x1440@165,3440x0,1,transform,1"
 
           # Notebook
           "eDP-1,2560x1600@240,0x0,1"
