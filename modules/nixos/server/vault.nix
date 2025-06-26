19780cb67cd9036982b3997893b95f38c3b7a631
sgiath <sgiath@sgiath.dev>
2025-06-26 21:35:09 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/server/vault.nix
+++ modules/nixos/server/vault.nix
@@ -3,11 +3,15 @@
   lib,
   ...
 }:
+let
+  secrets = builtins.fromJSON (builtins.readFile ./../../../secrets.json);
+in
 {
   config = lib.mkIf (config.sgiath.server.enable && config.services.vaultwarden.enable) {
     services.vaultwarden.config = {
       DOMAIN = "https://vault.sgiath.dev";
       SIGNUPS_ALLOWED = false;
+      ADMIN_TOKEN = secrets.vaultwarden_admin_token;
     
       ROCKET_ADDRESS = "127.0.0.1";
       ROCKET_PORT = 8222;
@@ -18,11 +22,13 @@
       # thus without transport encryption.
       # If you use an external mail server, follow:
       #   https://github.com/dani-garcia/vaultwarden/wiki/SMTP-configuration
-      SMTP_HOST = "127.0.0.1";
-      SMTP_PORT = 25;
-      SMTP_SSL = false;
+      SMTP_HOST = "smtp.protonmail.ch";
+      SMTP_PORT = 587;
+      SMTP_SECURITY = "force_tls";
+      SMTP_USERNAME = "FilipVavera@sgiath.dev";
+      SMTP_PASSWORD = secrets.protonmail_token;
 
-      SMTP_FROM = "vault@sgiath.dev";
+      SMTP_FROM = "FilipVavera@sgiath.dev";
       SMTP_FROM_NAME = "sgiath.dev Vault server";
     };
 
