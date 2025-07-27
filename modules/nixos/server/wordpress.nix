947e49699bacb24788444694d715873212f98a0a
sgiath <sgiath@sgiath.dev>
2025-07-27 10:33:09 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/server/wordpress.nix
+++ modules/nixos/server/wordpress.nix
@@ -10,22 +10,18 @@
     services = {
       nginx.virtualHosts."romana-vaverova.cz" = {
         # SSL
-        onlySSL = false;
+        onlySSL = true;
         kTLS = true;
 
         # ACME
-        enableACME = true;
-        acmeRoot = "/data/www/romana-vaverova.cz";
+        enableACME = false;
+        acmeRoot = null;
 
         # QUIC
         http3_hq = true;
         quic = true;
 
         locations = {
-          "/.well-known" = {
-            root = "/data/www/romana-vaverova.cz";
-          };
-
           "/" = {
             proxyPass = "http://127.0.0.1:8081";
           };
