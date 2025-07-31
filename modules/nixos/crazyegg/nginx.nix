066ec989a02399a5eca68c79bb51ec91b2c48e61
sgiath <sgiath@sgiath.dev>
2025-07-31 07:37:50 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/crazyegg/nginx.nix
+++ modules/nixos/crazyegg/nginx.nix
@@ -24,7 +24,6 @@
           proxyPass = "$target_destination";
           extraConfig = ''
             proxy_set_header CF-Connecting-IP $proxy_add_x_forwarded_for;
-            add_header Content-Security-Policy "default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; style-src * 'unsafe-inline'; frame-src *; connect-src *; font-src *; img-src *; media-src *;";
           '';
         };
       };
