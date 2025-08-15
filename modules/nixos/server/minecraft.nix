b7102aec377d0d87c52f7c06b81882bea128864a
sgiath <sgiath@sgiath.dev>
2025-08-15 19:02:43 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/server/minecraft.nix
+++ modules/nixos/server/minecraft.nix
@@ -6,18 +6,8 @@
 }:
 {
   config = lib.mkIf (config.sgiath.server.enable && config.services.minecraft-server.enable) {
-    environment = {
-      systemPackages = with pkgs; [
-        jdk8
-        jdk21
-      ];
-      
-      sessionVariables = {
-        JAVA8_HOME = "${pkgs.jdk8}/lib/openjdk";
-        JAVA21_HOME = "${pkgs.jdk21}/lib/openjdk";
-      };
-    };
-
+    
+    # vanila server
     services.minecraft-server = {
       eula = true;
       declarative = true;
@@ -31,6 +21,41 @@
         server-port = 25565;
         max-players = 10;
         online-mode = false;
+      };
+    };
+
+    systemd.services = {
+
+      # nomifactory (port 25566)
+      minecraft-nomi = {
+        enable = true;
+        description = "Minecraft Nomifactory server";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        environment = {
+          JAVA_PATH = "${pkgs.jdk8}/lib/openjdk/bin/java";
+        };
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "/data/minecraft/nomi/launch.sh";
+          WorkingDirectory = "/data/minecraft/nomi";
+        };
+      };
+
+      # GT: New Horizons (port 25567)
+      minecraft-gtnh = {
+        enable = true;
+        description = "Minecraft GTNH server";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        environment = {
+          JAVA_PATH = "${pkgs.jdk21}/lib/openjdk/bin/java";
+        };
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "/data/minecraft/gtnh/startserver-java9.sh";
+          WorkingDirectory = "/data/minecraft/gtnh";
+        };
       };
     };
   };
