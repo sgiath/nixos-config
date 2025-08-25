33017c0ace350eba734b00d4dfdbbdf70e3f820e
sgiath <sgiath@sgiath.dev>
2025-08-25 08:32:35 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/server/minecraft.nix
+++ modules/nixos/server/minecraft.nix
@@ -28,7 +28,7 @@ in
         description = "Minecraft Vanilla server";
         after = [ "network.target" ];
         wantedBy = [ "multi-user.target" ];
-        script = "${pkgs.jdk21}/lib/openjdk/bin/java -server -Xms4G -Xmx4G -jar paper-1.21.8-50.jar --nogui";
+        script = "${pkgs.jdk21}/lib/openjdk/bin/java -server -Xms4G -Xmx4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-1.21.8-50.jar --nogui";
         serviceConfig = {
           WorkingDirectory = "/data/minecraft/vanilla";
         };
