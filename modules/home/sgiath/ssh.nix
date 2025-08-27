c43789645e56a7b019f889d44954ec2362d9fb93
sgiath <sgiath@sgiath.dev>
2025-08-27 07:03:27 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/ssh.nix
+++ modules/home/sgiath/ssh.nix
@@ -51,19 +51,19 @@
           serverAliveInterval = 60;
           serverAliveCountMax = 30;
 
-          extraConfig = ''
-            Protocol 2
+          extraOptions = {
+            Protocol = 2;
     
-            PasswordAuthentication yes
-            ChallengeResponseAuthentication yes
-            PubkeyAuthentication yes
-            PreferredAuthentications publickey
-    
-            Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes256-ctr
-            KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
-            HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa
-            MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512,hmac-sha2-256
-          '';
+            PasswordAuthentication = true;
+            ChallengeResponseAuthentication = true;
+            PubkeyAuthentication = true;
+            PreferredAuthentications = "publickey";
+
+            Ciphers = "chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes256-ctr";
+            KexAlgorithms = "curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256";
+            HostKeyAlgorithms = "ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa";
+            MACs = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512,hmac-sha2-256";
+          };
         };
       };
     };
