930737db99e76283e2761528aa3543f391fae4b3
sgiath <sgiath@sgiath.dev>
2025-11-23 22:45:08 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/hyprland.nix
+++ modules/home/sgiath/hyprland.nix
@@ -28,10 +28,16 @@ in
       };
 
       settings = {
+        # colors
         general = {
           "col.active_border" = "rgba(F7DCDE39)";
           "col.inactive_border" = "rgba(A58A8D30)";
         };
+        misc.background_color = "rgba(1D1011FF)";
+        windowrule = ["bordercolor rgba(FFB2BCAA) rgba(FFB2BC77),pinned:1"];
+
+
+
 
         decoration.rounding = 5;
 
