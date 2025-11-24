a789e751f3be7fcfbf3d7dd8e95a51f8e2be4168
sgiath <sgiath@sgiath.dev>
2025-11-24 11:53:39 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/waybar.nix
+++ modules/home/sgiath/waybar.nix
@@ -18,24 +18,20 @@
           layer = "top";
           position = "top";
           height = 36;
-          spacing = 0;
+          spacing = 16;
           output = [
             "DP-1"
             "DP-3"
           ];
           modules-left = [
-            "custom/logo"
             "hyprland/workspaces"
           ];
           modules-right = [
-            "custom/kernel"
-            "custom/free-disk"
             "network"
             "network#2"
             "cpu"
             "memory"
             "clock"
-            "wireplumber"
             "tray"
           ];
 
@@ -53,19 +49,6 @@
           "hyprland/workspaces" = {
             disable-scroll = true;
             all-outputs = true;
-          };
-          "custom/kernel" = {
-            format = "{}";
-            interval = 3600;
-            exec = "uname -r";
-          };
-          "custom/logo" = {
-            format = "";
-          };
-          "custom/free-disk" = {
-            format = "󰆼 Disk: {}% used";
-            interval = 3600;
-            exec = "df -json | jq '.[] | select(.mount_point == \"/\")' | jq '(.free / .total * 1000 | round / 10.0)'";
           };
         };
         notebookBar = {
