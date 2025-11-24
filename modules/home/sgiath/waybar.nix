b60bac80968862482539b8474db11fd82d45bbf1
sgiath <sgiath@sgiath.dev>
2025-11-24 11:58:37 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/waybar.nix
+++ modules/home/sgiath/waybar.nix
@@ -45,7 +45,24 @@
           };
           memory.format = "RAM: {used} GiB / {total} GiB";
           cpu.format = "CPU: {usage}% ({max_frequency}GHz)";
-          clock.format = "{:%Y-%m-%d %H%M}";
+          clock = {
+            format = "{:%Y-%m-%d %H%M}";
+            interval = 1;
+            tooltip-format = "<tt><small>{calendar}</small></tt>";
+            calendar = {
+              mode = "year";
+              mode-mon-col = 3;
+              weeks-pos = "right";
+              on-scroll = 1;
+              format = {
+                months = "<span color='#ffead3'><b>{}</b></span>";
+                days = "<span color='#ecc6d9'><b>{}</b></span>";
+                weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+                weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+                today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+              };
+            };
+          };
           "hyprland/workspaces" = {
             disable-scroll = true;
             all-outputs = true;
