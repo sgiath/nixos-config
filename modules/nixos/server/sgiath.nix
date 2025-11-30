cbd0cd5c10191a1db58243f2e61af07c3be855a0
sgiath <sgiath@sgiath.dev>
2025-11-30 19:46:16 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/server/sgiath.nix
+++ modules/nixos/server/sgiath.nix
@@ -42,8 +42,9 @@
               add_header Cross-Origin-Resource-Policy 'cross-origin';
 
               default_type application/json;
+
+              return 200 '{"names":{"_":"0000002855ad7906a7568bf4d971d82056994aa67af3cf0048a825415ac90672"}}';
             '';
-            return = "200 '{\"names\":{\"_\":\"0000002855ad7906a7568bf4d971d82056994aa67af3cf0048a825415ac90672\"}}'";
           };
 
           "/profile".extraConfig = ''
