cece19366194e81aa0c3bc77a616633ffb892052
sgiath <sgiath@sgiath.dev>
2026-01-12 10:05:28 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/hyprland/layout.nix
+++ modules/home/sgiath/hyprland/layout.nix
@@ -6,7 +6,7 @@
       force_split = 2;
       preserve_split = true;
       smart_split = true;
-      split_width_multiplier = 2.0;
+      split_width_multiplier = 0.5;
       default_split_ratio = 0.5;
     };
 
