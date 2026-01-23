0c8106dd6a7d591c8ace04a114eb228a5ea321a5
sgiath <sgiath@sgiath.dev>
2026-01-23 14:29:28 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- packages/coderabbit/default.nix
+++ packages/coderabbit/default.nix
@@ -3,7 +3,8 @@
   stdenv,
   fetchurl,
   unzip,
-  autoPatchelfHook,
+  glibc,
+  patchelf,
 }:
 
 stdenv.mkDerivation rec {
@@ -15,7 +16,7 @@ stdenv.mkDerivation rec {
     hash = "sha256-2j4wR6QJ36GsnN7yrBgvCWWb6l7DS29v4Sl4T16H7po=";
   };
 
-  nativeBuildInputs = [ unzip autoPatchelfHook ];
+  nativeBuildInputs = [ unzip patchelf ];
 
   unpackPhase = ''
     unzip $src
@@ -23,8 +24,16 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     install -Dm755 coderabbit $out/bin/coderabbit
+
+    # Patch the interpreter only - do not use autoPatchelfHook as it breaks Bun-compiled binaries
+    patchelf --set-interpreter "${glibc}/lib/ld-linux-x86-64.so.2" $out/bin/coderabbit
+
     ln -s $out/bin/coderabbit $out/bin/cr
   '';
+
+  # Don't strip or modify the binary further
+  dontStrip = true;
+  dontPatchELF = true;
 
   meta = with lib; {
     description = "AI-powered code review CLI tool";
