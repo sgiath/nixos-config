c55034a8eb83a6a1b182d855646fbe8c9c93c1f2
sgiath <sgiath@sgiath.dev>
2026-01-23 10:01:59 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/sgiath/wayland.nix
+++ modules/nixos/sgiath/wayland.nix
@@ -67,9 +67,9 @@
       hyprland = {
         enable = true;
         # set the flake package
-        package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+        # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
         # make sure to also set the portal package, so that they are in sync
-        portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+        # portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
       };
 
       hyprlock.enable = true;
