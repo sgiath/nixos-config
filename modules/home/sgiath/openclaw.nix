37afbe550c90bc68e1704c352bcc1d4b156e36fe
sgiath <sgiath@sgiath.dev>
2026-01-31 19:17:19 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/openclaw.nix
+++ modules/home/sgiath/openclaw.nix
@@ -1,32 +1,25 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 {
   config = lib.mkIf config.programs.openclaw.enable {
     programs.openclaw = {
       documents = ./openclaw;
-      # excludeTools = [
-      #   "git"
-      #   "jq"
-      #   "ripgrep"
-      # ];
-      # toolNames = [ ];
 
-      # firstParty = {
-      #   summarize.enable = true; # Summarize web pages, PDFs, videos
-      #   peekaboo.enable = true; # Take screenshots
-      #   oracle.enable = false; # Web search
-      #   poltergeist.enable = false; # Control your macOS UI
-      #   sag.enable = false; # Text-to-speech
-      #   camsnap.enable = false; # Camera snapshots
-      #   gogcli.enable = false; # Google Calendar
-      #   bird.enable = false; # Twitter/X
-      #   sonoscli.enable = false; # Sonos control
-      #   imsg.enable = false; # iMessage
-      # };
+      firstParty = {
+        summarize.enable = true; # Summarize web pages, PDFs, videos
+        peekaboo.enable = true; # Take screenshots
+        oracle.enable = true; # Web search
+        poltergeist.enable = false; # Control your macOS UI
+        sag.enable = true; # Text-to-speech
+        camsnap.enable = true; # Camera snapshots
+        gogcli.enable = false; # Google Calendar
+        bird.enable = true; # Twitter/X
+        sonoscli.enable = false; # Sonos control
+        imsg.enable = false; # iMessage
+      };
 
       config = {
         channels.telegram = {
@@ -38,7 +31,6 @@
       instances.default = {
         enable = true;
         launchd.enable = true;
-        # package = pkgs.openclawPackages.withTools { ... }.openclaw;
 
         stateDir = "/home/sgiath/.openclaw";
         workspaceDir = "/home/sgiath/.openclaw/workspace";
