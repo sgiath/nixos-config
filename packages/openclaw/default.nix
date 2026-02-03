66e6a983b506650bb53d48543a94103cee8700e9
sgiath <sgiath@sgiath.dev>
2026-02-03 15:17:35 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- packages/openclaw/default.nix
+++ packages/openclaw/default.nix
@@ -14,6 +14,12 @@ buildNpmPackage rec {
     hash = "sha256-63N/tepPdx7RGzhw/w5y/JeLcj+UQ4L9FseES41p6gM=";
   };
 
+  # Prebuilt native binary for matrix-sdk-crypto (skipped by --ignore-scripts)
+  matrixCryptoNative = fetchurl {
+    url = "https://github.com/matrix-org/matrix-rust-sdk-crypto-nodejs/releases/download/v0.4.0/matrix-sdk-crypto.linux-x64-gnu.node";
+    hash = "sha256-cHjU3ZhxKPea/RksT2IfZK3s435D8qh1bx0KnwNN5xg=";
+  };
+
   sourceRoot = "package";
 
   postPatch = ''
@@ -31,6 +37,11 @@ buildNpmPackage rec {
   npmFlags = [ "--ignore-scripts" ];
 
   makeCacheWritable = true;
+
+  postInstall = ''
+    # Install prebuilt matrix-sdk-crypto native binary
+    cp $matrixCryptoNative $out/lib/node_modules/openclaw/node_modules/@matrix-org/matrix-sdk-crypto-nodejs/matrix-sdk-crypto.linux-x64-gnu.node
+  '';
 
   meta = {
     description = "WhatsApp gateway CLI (Baileys web) with Pi RPC agent";
