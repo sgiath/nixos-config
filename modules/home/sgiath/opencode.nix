82034aa9b6ab51199e1b94d44a3819c1b4ca1307
sgiath <sgiath@sgiath.dev>
2026-02-03 11:46:39 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/opencode.nix
+++ modules/home/sgiath/opencode.nix
@@ -16,44 +16,45 @@ in
 
     # aliases
     programs.zsh.shellAliases = {
-      oc = "OPENCODE_DISABLE_CLAUDE_CODE=true OPENCODE_CONFIG=${config.xdg.configHome}/opencode/vanilla/opencode.jsonc OPENCODE_CONFIG_DIR=${config.xdg.configHome}/opencode/vanilla ${opencode}/bin/opencode attach http://localhost:4096 --dir $(pwd)";
-      omo = "OPENCODE_DISABLE_CLAUDE_CODE=true OPENCODE_CONFIG=${config.xdg.configHome}/opencode/omo/opencode.jsonc OPENCODE_CONFIG_DIR=${config.xdg.configHome}/opencode/omo ${opencode}/bin/opencode attach http://localhost:4097 --dir $(pwd)";
+      oc = "OPENCODE_EXPERIMENTAL_PLAN_MODE=1 OPENCODE_DISABLE_CLAUDE_CODE=true OPENCODE_CONFIG=${config.xdg.configHome}/opencode/vanilla/opencode.jsonc OPENCODE_CONFIG_DIR=${config.xdg.configHome}/opencode/vanilla ${opencode}/bin/opencode --port 4096";
+      omo = "OPENCODE_DISABLE_CLAUDE_CODE=true OPENCODE_CONFIG=${config.xdg.configHome}/opencode/omo/opencode.jsonc OPENCODE_CONFIG_DIR=${config.xdg.configHome}/opencode/omo ${opencode}/bin/opencode --port 4097";
     };
 
     systemd.user.services = {
-      opencode-vanilla = {
-        Unit.Description = "OpenCode vanilla Server";
-        Service = {
-          Environment = [
-            "OPENCODE_SERVER_PASSWORD=\"\""
-            "OPENCODE_DISABLE_CLAUDE_CODE=true"
-            "OPENCODE_CONFIG=${config.xdg.configHome}/opencode/vanilla/opencode.jsonc"
-            "OPENCODE_CONFIG_DIR=${config.xdg.configHome}/opencode/vanilla"
-          ];
-          ExecStart = "${opencode}/bin/opencode serve --port 4096";
-        };
-        Install = {
-          WantedBy = [ "multi-user.target" ];
-        };
-      };
+      # opencode-vanilla = {
+      #   Unit.Description = "OpenCode vanilla Server";
+      #   Service = {
+      #     Environment = [
+      #       "OPENCODE_SERVER_PASSWORD=\"\""
+      #       "OPENCODE_EXPERIMENTAL_PLAN_MODE=1"
+      #       "OPENCODE_DISABLE_CLAUDE_CODE=true"
+      #       "OPENCODE_CONFIG=${config.xdg.configHome}/opencode/vanilla/opencode.jsonc"
+      #       "OPENCODE_CONFIG_DIR=${config.xdg.configHome}/opencode/vanilla"
+      #     ];
+      #     ExecStart = "${opencode}/bin/opencode serve --port 4096";
+      #   };
+      #   Install = {
+      #     WantedBy = [ "multi-user.target" ];
+      #   };
+      # };
 
-      opencode-omo = {
-        Unit = {
-          Description = "OpenCode oh-my-opencode Server";
-        };
-        Service = {
-          Environment = [
-            "OPENCODE_SERVER_PASSWORD=\"\""
-            "OPENCODE_DISABLE_CLAUDE_CODE=true"
-            "OPENCODE_CONFIG=${config.xdg.configHome}/opencode/omo/opencode.jsonc"
-            "OPENCODE_CONFIG_DIR=${config.xdg.configHome}/opencode/omo"
-          ];
-          ExecStart = "${opencode}/bin/opencode serve --port 4097";
-        };
-        Install = {
-          WantedBy = [ "multi-user.target" ];
-        };
-      };
+      #   opencode-omo = {
+      #     Unit = {
+      #       Description = "OpenCode oh-my-opencode Server";
+      #     };
+      #     Service = {
+      #       Environment = [
+      #         "OPENCODE_SERVER_PASSWORD=\"\""
+      #         "OPENCODE_DISABLE_CLAUDE_CODE=true"
+      #         "OPENCODE_CONFIG=${config.xdg.configHome}/opencode/omo/opencode.jsonc"
+      #         "OPENCODE_CONFIG_DIR=${config.xdg.configHome}/opencode/omo"
+      #       ];
+      #       ExecStart = "${opencode}/bin/opencode serve --port 4097";
+      #     };
+      #     Install = {
+      #       WantedBy = [ "multi-user.target" ];
+      #     };
+      #   };
     };
   };
 }
