448efa3eddda30c5cec59e41b65a6baca976a060
sgiath <sgiath@sgiath.dev>
2026-02-05 22:09:27 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- flake.nix
+++ flake.nix
@@ -86,7 +86,7 @@
 
     # https://github.com/openai/codex/releases/latest
     codex = {
-      url = "github:openai/codex/rust-v0.98.0";
+      url = "github:openai/codex/rust-v0.89.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
