bf9bfe0ef9caf55cf9e45790241085ece362ee26
sgiath <sgiath@sgiath.dev>
2026-02-05 16:22:11 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- homes/x86_64-linux/sgiath@ceres/default.nix
+++ homes/x86_64-linux/sgiath@ceres/default.nix
@@ -1,4 +1,21 @@
-{ pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}:
+let
+  secrets = builtins.fromJSON (builtins.readFile ./../../../secrets.json);
+
+  openclawPath = lib.concatStringsSep ":" [
+    "${config.home.profileDirectory}/bin"
+    "/run/current-system/sw/bin"
+    "${pkgs.coreutils}/bin"
+    "${pkgs.curl}/bin"
+    "${pkgs.yt-dlp}/bin"
+  ];
+in
 {
   home.packages = with pkgs; [
     gimp
@@ -24,5 +41,34 @@
     desktop = 10;
     popups = 10;
     terminal = 10;
+  };
+
+  systemd.user.services = {
+    openclaw-gateway = {
+      Unit = {
+        Description = "OpenClaw Node";
+        After = [ "network-online.target" ];
+        Wants = [ "network-online.target" ];
+      };
+      Service = {
+        Restart = "always";
+        RestartSec = 5;
+        KillMode = "process";
+        Environment = [
+          "HOME=/home/sgiath"
+          "PATH=${openclawPath}"
+          "OPENCLAW_GATEWAY_PORT=18789"
+          "OPENCLAW_GATEWAY_TOKEN=${secrets.openclaw-token}"
+          "OPENCLAW_SYSTEMD_UNIT=openclaw-gateway.service"
+          "OPENCLAW_SERVICE_MARKER=openclaw"
+          "OPENCLAW_SERVICE_KIND=gateway"
+          "OPENCLAW_SERVICE_VERSION=2026.2.3"
+        ];
+        ExecStart = "${pkgs.${namespace}.openclaw}/bin/openclaw node run --host 192.168.1.3 --port 18789";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
   };
 }
