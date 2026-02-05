76faecddefc919ce9f24ca757ab6624c88482ec9
sgiath <sgiath@sgiath.dev>
2026-02-05 12:33:52 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- flake.nix
+++ flake.nix
@@ -136,6 +136,7 @@
         cudaSupport = false;
         rocmSupport = false;
         allowUnfree = true;
+        allowBroken = true;
         permittedInsecurePackages = [
           "jitsi-meet-1.0.8043"
           "electron-36.9.5"
