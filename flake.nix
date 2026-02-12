33c0ae6291da0786e2a5b65dd8a4187ebac36466
sgiath <sgiath@sgiath.dev>
2026-02-12 07:30:06 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- flake.nix
+++ flake.nix
@@ -92,7 +92,7 @@
 
     # https://github.com/anomalyco/opencode/releases/latest
     opencode = {
-      url = "github:anomalyco/opencode";
+      url = "github:anomalyco/opencode/v1.1.60";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
