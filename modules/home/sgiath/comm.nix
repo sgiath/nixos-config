9bc01a174fb33f1b799d6c953e11d1771b22b471
sgiath <sgiath@sgiath.dev>
2026-02-18 12:50:34 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/comm.nix
+++ modules/home/sgiath/comm.nix
@@ -23,6 +23,38 @@
       pkgs.${namespace}.nak
     ];
 
+    programs = {
+      element-desktop = {
+        enable = true;
+        settings = {
+          default_server_config = {
+            "m.homeserver" = {
+              base_url = "https://matrix.sgiath.dev";
+              server_name = "sgiath.dev";
+            };
+          };
+
+          features = {
+            feature_latex_maths = true;
+            feature_pinning = true;
+            feature_dm_verification = true;
+            feature_location_share_live = true;
+            feature_video_rooms = true;
+            feature_element_call_video_rooms = true;
+            feature_group_calls = true;
+            feature_new_room_list = true;
+          };
+
+          disable_custom_urls = false;
+          disable_login_language_selector = false;
+          force_verification = true;
+
+          default_theme = "dark";
+          brand = "sgiath";
+        };
+      };
+    };
+
     wayland.windowManager.hyprland.settings = {
       exec-once = [
         "${pkgs.webcord}/bin/webcord"
