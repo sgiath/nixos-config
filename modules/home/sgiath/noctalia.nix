0ce682c03b7b24943e9c9147d2ba7c6bf6d46a2b
sgiath <sgiath@sgiath.dev>
2026-03-22 20:51:51 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/noctalia.nix
+++ modules/home/sgiath/noctalia.nix
@@ -12,13 +12,15 @@
       # settings = { };
     };
     wayland.windowManager.hyprland.settings = {
-      layerrule = {
-        name = "noctalia";
-        "match:namespace" = "noctalia-background-.*$";
-        ignore_alpha = 0.5;
-        blur = true;
-        blur_popups = true;
-      };
+      layerrule = [
+        {
+          name = "noctalia";
+          "match:namespace" = "noctalia-background-.*$";
+          ignore_alpha = 0.5;
+          blur = true;
+          blur_popups = true;
+        }
+      ];
     };
   };
 }
