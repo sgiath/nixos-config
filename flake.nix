67ffc68e5cf562bc913e60c7f7acf28c2602d1b4
sgiath <sgiath@sgiath.dev>
2026-04-02 10:01:30 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- flake.nix
+++ flake.nix
@@ -111,7 +111,7 @@
 
     # https://github.com/anomalyco/opencode/releases/latest
     opencode = {
-      url = "github:anomalyco/opencode";
+      url = "github:anomalyco/opencode/0f488996b30780f6fe9ecd08d2b3701024b5eea3";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
