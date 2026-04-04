fb26b98b7fcf50a8d43f2b86929bad13f3d553a2
sgiath <sgiath@sgiath.dev>
2026-04-04 15:45:46 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- packages/claude-agent-acp/default.nix
+++ packages/claude-agent-acp/default.nix
@@ -6,16 +6,16 @@
 
 buildNpmPackage rec {
   pname = "claude-agent-acp";
-  version = "0.24.2";
+  version = "0.25.0";
 
   src = fetchFromGitHub {
     owner = "agentclientprotocol";
     repo = "claude-agent-acp";
     rev = "v${version}";
-    hash = "sha256-SRVbLcGrH5pJt6yfM0ObSso68M+yGateIVYf/kFVDhE=";
+    hash = "sha256-RaVBdFnga/3Ra1EW8v2K/vl65vTK8k8MZFjzFyYQk/k=";
   };
 
-  npmDepsHash = "sha256-V5lBQNhpL+/Mok9bEVSOrrHSv9B9pXKJswcXW+QDnAs=";
+  npmDepsHash = "sha256-JZPbnB1Tpua7t4tZZf4Q/LNpOBl5A77TYgYQPB9j0Vk=";
 
   # Build TypeScript to dist/
   npmBuildScript = "build";
