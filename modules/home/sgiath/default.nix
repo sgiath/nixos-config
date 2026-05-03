2e193ee5c873f781749703f4878fe8f0f539b0f6
sgiath <sgiath@sgiath.dev>
2026-05-03 17:16:17 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/default.nix
+++ modules/home/sgiath/default.nix
@@ -49,7 +49,7 @@
               ;;
 
             --vesta)
-              nix-store --add-fixed sha256 ~/nix-root/FoundryVTT-Linux-14.359.zip
+              nix-store --add-fixed sha256 ~/nix-root/FoundryVTT-Linux-14.360.zip
               NIX_SSHOPTS="-o IdentityAgent=$SSH_AUTH_SOCK" nixos-rebuild switch --sudo --flake '.#vesta' --target-host 'vesta.local'
               ;;
 
