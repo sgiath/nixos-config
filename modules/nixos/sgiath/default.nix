be89b61bd0fb26964009947dc1caa54870628ed1
sgiath <sgiath@sgiath.dev>
2026-05-04 16:34:47 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/sgiath/default.nix
+++ modules/nixos/sgiath/default.nix
@@ -96,6 +96,7 @@ in
       ANTHROPIC_API_KEY = secrets.anthropic;
       OPENROUTER_API_KEY = secrets.openrouter;
       GITHUB_PERSONAL_ACCESS_TOKEN = secrets.github_token;
+      LINEAR_API_KEY = secrets.linear_api_key;
     };
     programs = {
       nix-ld.enable = true;
