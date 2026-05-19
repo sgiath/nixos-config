2c109d5a0d82d3dd53bfc6c78672c44c676d9ac0
sgiath <sgiath@sgiath.dev>
2026-05-19 13:20:16 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/sgiath/security.nix
+++ modules/nixos/sgiath/security.nix
@@ -55,14 +55,14 @@
         "ssh-ed25519"
         "ssh-rsa"
       ];
+      macs = [
+        "hmac-sha2-512-etm@openssh.com"
+        "hmac-sha2-256-etm@openssh.com"
+        "umac-128-etm@openssh.com"
+        "hmac-sha2-512"
+        "hmac-sha2-256"
+        "umac-128@openssh.com"
+      ];
     };
-    macs = [
-      "hmac-sha2-512-etm@openssh.com"
-      "hmac-sha2-256-etm@openssh.com"
-      "umac-128-etm@openssh.com"
-      "hmac-sha2-512"
-      "hmac-sha2-256"
-      "umac-128@openssh.com"
-    ];
   };
 }
