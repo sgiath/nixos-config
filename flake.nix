3bff33fa0817dba7e03b78389162c76b141c4a75
sgiath <sgiath@sgiath.dev>
2026-05-22 09:45:51 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- flake.nix
+++ flake.nix
@@ -89,7 +89,7 @@
     };
 
     # https://github.com/anomalyco/opencode/releases/latest
-    opencode.url = "github:anomalyco/opencode/v1.15.7";
+    opencode.url = "github:anomalyco/opencode/v1.15.4";
 
     openspec = {
       url = "github:Fission-AI/OpenSpec";
