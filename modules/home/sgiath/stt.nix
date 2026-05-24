2f5ca03933c67b496395746fe1f476770190a0de
sgiath <sgiath@sgiath.dev>
2026-05-24 08:41:26 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/stt.nix
+++ modules/home/sgiath/stt.nix
@@ -39,9 +39,9 @@ in
     };
 
     systemd.user.services.voxtype = {
-      Service.Environment = [
-        "VOXTYPE_VULKAN_DEVICE=amd"
-      ];
+      Service.Environment = {
+        VOXTYPE_VULKAN_DEVICE = "amd";
+      };
     };
   };
 }
