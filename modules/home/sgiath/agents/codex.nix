6fefd868bee2fdaa6af52abec18f62f6bf6a8131
sgiath <sgiath@sgiath.dev>
2026-06-05 08:23:47 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/agents/codex.nix
+++ modules/home/sgiath/agents/codex.nix
@@ -9,9 +9,10 @@
     # intentionally not using codex module since codex really wants to update its config in place
     # and managing it through Nix is more pain then benefits
 
-    home.packages = [ pkgs.llm-agents.codex ];
-
-    file.".codex/AGENTS.md".source = ./AGENTS.md;
+    home = {
+      packages = [ pkgs.llm-agents.codex ];
+      file.".codex/AGENTS.md".source = ./AGENTS.md;
+    };
 
     programs.zsh.shellAliases = {
       cx = "${lib.getExe pkgs.llm-agents.codex} --yolo";
