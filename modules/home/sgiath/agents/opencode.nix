8380fbecf9248a18166e6634b3ec1d7c7b81ef78
sgiath <sgiath@sgiath.dev>
2026-06-15 19:08:13 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/agents/opencode.nix
+++ modules/home/sgiath/agents/opencode.nix
@@ -6,55 +6,15 @@
 }:
 {
   config = lib.mkIf config.sgiath.agents.enable {
-    programs.opencode = {
-      enable = true;
-      package = pkgs.llm-agents.opencode;
-      enableMcpIntegration = true;
-      context = ./AGENTS.md;
-      settings = {
-        autoupdate = false;
-        permission = {
-          bash = {
-            "*" = "allow";
-            "aws *" = "ask";
-            "kubectl exec *" = "ask";
-          };
-          read = {
-            "/nix/store/**" = "allow";
-          };
-          edit = {
-            "/tmp/**" = "allow";
-            "~/**" = "allow";
-          };
-          external_directory = {
-            "/tmp/**" = "allow";
-            "~/**" = "allow";
-          };
-        };
-        plugin = [
-          "@plannotator/opencode@latest"
-        ];
-        server = {
-          hostname = "0.0.0.0";
-          mdns = true;
-          cors = [
-            "http://localhost:4096"
-            "http://192.168.1.7:4096"
-            "http://localhost:4097"
-            "http://192.168.1.7:4097"
-          ];
-        };
-        experimental = {
-          batch_tool = true;
-        };
-      };
+    # intentionally not using opencode module since opencode really wants to update its config
+    # in place and managing it through Nix is more pain then benefits
 
-      web.enable = true;
-    };
+    home.packages = [ pkgs.llm-agents.opencode ];
+    xdg.configFile."opencode/AGENTS.md".source = ./AGENTS.md;
     stylix.targets.opencode.enable = false;
 
     programs.zsh.shellAliases = {
-      oc = "OPENCODE_EXPERIMENTAL_WORKSPACES=true ${lib.getExe pkgs.llm-agents.opencode}";
+      oc = "${lib.getExe pkgs.llm-agents.opencode}";
     };
   };
 }
