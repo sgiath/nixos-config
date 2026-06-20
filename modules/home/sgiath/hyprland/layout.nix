058dddd0f08dcd09a80b4c27b1ced3c86a5be3d8
sgiath <sgiath@sgiath.dev>
2026-06-20 06:29:28 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/hyprland/layout.nix
+++ modules/home/sgiath/hyprland/layout.nix
@@ -5,18 +5,7 @@
     dwindle = {
       force_split = 2;
       preserve_split = true;
-      smart_split = true;
-      split_width_multiplier = 0.5;
-      default_split_ratio = 0.5;
-    };
-
-    master = {
-      mfact = 0.35;
-      new_status = "master";
-      orientation = "center";
-      slave_count_for_center_master = 1;
-      center_master_fallback = "right";
-      always_keep_position = true;
+      default_split_ratio = 0.65;
     };
   };
 }
