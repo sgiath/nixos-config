1bbb6d80a5130fc85136ada34dfc0e5833bb29fd
sgiath <sgiath@sgiath.dev>
2026-06-26 09:19:12 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- flake.nix
+++ flake.nix
@@ -97,7 +97,7 @@
     llm-agents.url = "github:numtide/llm-agents.nix";
 
     hermes-agent = {
-      url = "github:NousResearch/hermes-agent";
+      url = "github:NousResearch/hermes-agent/v2026.6.5";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
