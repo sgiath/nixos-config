1352c361f02930f1a81981c7edb4417623a166f5
sgiath <sgiath@sgiath.dev>
2026-07-13 10:53:14 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/agents/cursor.nix
+++ modules/home/sgiath/agents/cursor.nix
@@ -8,6 +8,10 @@
   config = lib.mkIf config.sgiath.agents.enable {
     home.packages = [ pkgs.llm-agents.cursor-agent ];
 
+    programs.cursor = {
+      enable = true;
+    };
+
     programs.zsh.shellAliases = {
       ca = "${lib.getExe pkgs.llm-agents.cursor-agent} --yolo --approve-mcps --trust";
     };
