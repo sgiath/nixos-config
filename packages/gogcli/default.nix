fb6022e29c1fcc079914bf85b439b9fb08e454d5
sgiath <sgiath@sgiath.dev>
2026-07-17 07:04:03 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- packages/gogcli/default.nix
+++ packages/gogcli/default.nix
@@ -6,13 +6,13 @@
 
 buildGo127Module rec {
   pname = "gogcli";
-  version = "0.34.0";
+  version = "0.34.1";
 
   src = fetchFromGitHub {
     owner = "steipete";
     repo = "gogcli";
     rev = "v${version}";
-    hash = "sha256-3iVxq8AIzRebHiHxcagJaKKfqWKPiiKG4GMczm+AODU=";
+    hash = "sha256-ocC+A63GLrZQA3mXEPjIfsM7Af9NiYjGF8DZI/2yNS0=";
   };
 
   vendorHash = "sha256-MAbbCLdoOLWer7HO3+RZJuN10gLeTgN6/CbNn6pzGwQ=";
